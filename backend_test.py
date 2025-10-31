a8840244d9bf3997795a574bfc4ec1156a42905a
Sindhu N <sindhunarayan2252@gmail.com>
2025-10-31 12:31:07 +0000

    auto-commit for c5e20c91-b453-4375-a8ba-2c9dc8d3d84a

--- backend_test.py
+++ backend_test.py
@@ -19,7 +19,7 @@ def get_backend_url():
                     return line.split('=', 1)[1].strip()
     except:
         pass
-    return "https://talent-nexus-25.preview.emergentagent.com"
+    return "https://venuemate-18.preview.emergentagent.com"
 
 BASE_URL = get_backend_url()
 API_BASE = f"{BASE_URL}/api"
